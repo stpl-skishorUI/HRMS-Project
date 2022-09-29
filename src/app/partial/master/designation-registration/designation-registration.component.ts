5b27b00b2b2de12ac18494a343be689092f1820c
Sanket <panchmind1@gmail.com>
2022-09-29 16:50:52 +0530

    add designation page design done

--- src/app/partial/master/designation-registration/designation-registration.component.ts
+++ src/app/partial/master/designation-registration/designation-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { AddDesignationComponent } from './add-designation/add-designation.component';
 
 @Component({
   selector: 'app-designation-registration',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignationRegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
 
+  addDesignation() {
+    const dialogRef = this.dialog.open(AddDesignationComponent,{
+      width: '30%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+
+  displayedColumns: string[] = ['srno', 'designation_name', 'department_name', 'company','action'];
+  dataSource = ELEMENT_DATA;
 }
+export interface PeriodicElement {
+  srno: number;
+  designation_name: string;
+  department_name: string;
+  company: string;
+  action: any;
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, designation_name: 'Html Designer', department_name: 'Software Development', company: 'shaurya',action:''}
+];
