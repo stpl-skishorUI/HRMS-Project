79df12868489ada76132915a27b01fca568dbbc7
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-09-30 18:02:52 +0530

    comment

--- src/app/core/services/call-api.service.ts
+++ src/app/core/services/call-api.service.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
 })
 export class CallApiService {
   userObj = new Array();
-//check122
   private httpObj: any = {
     type: '',
     url: '',
@@ -18,7 +17,7 @@ export class CallApiService {
 
   getBaseurl(url: string) {
     switch (url) {
-      case 'base': return 'http://hrmssvr.erpguru.in/api/'; break;
+      case 'baseUrlCheck1': return 'http://hrmssvr.erpguru.in/api/'; break;
       default: return ''; break;         
     }
   }
