3cb779591d5862f1bba7f3886061246c729e092e
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-09-30 13:32:33 +0530

    commit change

--- src/app/core/services/call-api.service.ts
+++ src/app/core/services/call-api.service.ts
@@ -18,7 +18,7 @@ export class CallApiService {
 
   getBaseurl(url: string) {
     switch (url) {
-      case 'Documype': return 'http://hrmssvr.erpguru.in/api/'; break;
+      case 'Documype1': return 'http://hrmssvr.erpguru.in/api/'; break;
       default: return ''; break;         
     }
   }
