f260a8b0991c75fc4079d8db4513cbfdbc9b5b95
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-09-30 13:32:08 +0530

    comment

--- src/app/core/services/call-api.service.ts
+++ src/app/core/services/call-api.service.ts
@@ -18,7 +18,7 @@ export class CallApiService {
 
   getBaseurl(url: string) {
     switch (url) {
-      case 'Documype': return 'http://hrmssvr.erpguru.in/api/'; break;
+      case 'baseURL': return 'http://hrmssvr.erpguru.in/api/'; break;
       default: return ''; break;         
     }
   }
