7260558c07d7fefaaf158053756c15f12f91098e
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-09-30 17:01:37 +0530

    baseUrl Check

--- src/app/core/services/call-api.service.ts
+++ src/app/core/services/call-api.service.ts
@@ -18,7 +18,7 @@ export class CallApiService {
 
   getBaseurl(url: string) {
     switch (url) {
-      case 'baseURL': return 'http://hrmssvr.erpguru.in/api/'; break;
+      case 'baseUrlCheck': return 'http://hrmssvr.erpguru.in/api/'; break;
       default: return ''; break;         
     }
   }
