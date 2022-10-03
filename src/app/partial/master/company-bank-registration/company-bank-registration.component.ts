3789e07827c525644c0f4867701ee16c5801ef04
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-10-03 18:55:30 +0530

    com-bank-registration upload

--- src/app/partial/master/company-bank-registration/company-bank-registration.component.ts
+++ src/app/partial/master/company-bank-registration/company-bank-registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { CallApiService } from 'src/app/core/services/call-api.service';
 import { AddCompanyBankRegistrationComponent } from './add-company-bank-registration/add-company-bank-registration.component';
 
 @Component({
@@ -8,23 +10,117 @@ import { AddCompanyBankRegistrationComponent } from './add-company-bank-registra
   styleUrls: ['./company-bank-registration.component.scss']
 })
 export class CompanyBankRegistrationComponent implements OnInit {
-
-  displayedColumns: string[] = ['sr_no', 'LeaveType_Name', 'Half_Day','Branch_Name','Account_No',];
+  filterForm!:FormGroup
+  displayedColumns: string[] = ['sr_no', 'companyName', 'bankName','branchName','accountNo','Action'];
   dataSource = ELEMENT_DATA;
+  campanyNameArray = new Array();
+  bankNameArray = new Array();
+  branchNameArray = new Array();
+  accountTypeArray = ['saving','current'];
 
-  constructor(public dialog: MatDialog) {}
+  sendObj ={
+    CompanyId:0,
+    BankId:0,
+    BranchId:0,
+    AccountType:''
+  }
+
+  constructor(public dialog: MatDialog, private api: CallApiService, private fb:FormBuilder) {}
 
   ngOnInit(): void {
+    this.filterFormData();
+    this.getCampanyNameDropdown();
+    this.getBankNameDropdown();
+    this.getBranchNameDropdown();
+    this.blindTableData()
   }
-  openDialog() {
+  openDialog(editObj?:any) {
     const dialogRef = this.dialog.open(AddCompanyBankRegistrationComponent,{
-      width:'40%'
+      width:'40%',
+      data: editObj
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      this.blindTableData();
+      // console.log(`Dialog result: ${result}`);
     });
   }
+
+  filterFormData(){
+    this.filterForm = this.fb.group({
+      companyId :[' '],
+      bankId :[' '],
+      branchId :[' '],
+      accountType :[' '],
+    })    
+  }
+
+  // --------------------------------------------Blind Table Data-------------------------------------------
+  
+  blindTableData(){
+    this.api.setHttp('get','api/CompanyBankAccount/GetAllCompanyBankAccountDetails?CompanyId='+this.sendObj.CompanyId+'&BankId='+this.sendObj.BankId+'&BranchId='+this.sendObj.BranchId+'&AccountType='+this.sendObj.AccountType,
+    false,false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : ((res:any)=>{
+        if(res.statusCode === '200' && res.responseData.length){
+          this.dataSource = res.responseData;
+        }
+        else{
+          this.dataSource = []
+        }
+      })
+    })
+  }
+
+  // --------------------------------------------Blind Table Data-------------------------------------------
+
+   // --------------------------------------------Dropdown Start-------------------------------------------
+
+  getCampanyNameDropdown(){
+    this.api.setHttp('get','api/CommonDropDown/GetCompany',false,false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : ((res:any)=>{
+        if(res.statusCode === '200' && res.responseData.length){
+          this.campanyNameArray = res.responseData;          
+        }
+      })
+    })
+  }
+
+  getBankNameDropdown(){
+    this.api.setHttp('get','api/CommonDropDown/GetBankRegistration',false,false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : ((res:any)=>{
+        if(res.statusCode === '200' && res.responseData.length){
+          this.bankNameArray = res.responseData;
+        }
+      })
+    })
+  }
+
+  getBranchNameDropdown(){
+    this.api.setHttp('get','api/CommonDropDown/GetBankBranchRegistration',false,false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : ((res:any)=>{
+        if(res.statusCode === '200' && res.responseData.length){
+          this.branchNameArray = res.responseData;
+        }
+      })
+    })
+  }
+
+// --------------------------------------------Dropdown End-------------------------------------------
+
+FilterFormSubmit(){
+  let obj = this.filterForm.value;
+  this.sendObj.CompanyId = obj.companyId ? obj.companyId: 0;
+  this.sendObj. BankId  = obj.bankId ? obj.bankId: 0;
+  this.sendObj.BranchId  = obj.branchId ? obj.branchId: 0;
+  this.sendObj. AccountType  = obj.accountType ? obj.accountType: '';
+ this.blindTableData()
+  
+}
+
 
 }
 const ELEMENT_DATA: PeriodicElement[] = [
