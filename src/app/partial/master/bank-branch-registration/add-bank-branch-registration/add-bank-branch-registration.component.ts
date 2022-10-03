ea2b80d01cd3b2cb1759b123f6f2e21ecb71fccf
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2022-10-03 18:51:08 +0530

    bank-branch-reg update

--- src/app/partial/master/bank-branch-registration/add-bank-branch-registration/add-bank-branch-registration.component.ts
+++ src/app/partial/master/bank-branch-registration/add-bank-branch-registration/add-bank-branch-registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CallApiService } from 'src/app/core/services/call-api.service';
 
 @Component({
@@ -9,22 +11,76 @@ import { CallApiService } from 'src/app/core/services/call-api.service';
 })
 export class AddBankBranchRegistrationComponent implements OnInit {
 
-  displayedColumns: string[] = ['sr_no', 'Bank_Name','action'];
+  displayedColumns: string[] = ['sr_no', 'Bank_Name', 'action'];
   dataSource = ELEMENT_DATA;
+  bankForm!: FormGroup;
+  editFlag:boolean = false;
+  editObj :any;
+  constructor(private fb: FormBuilder, private api: CallApiService, private mat: MatSnackBar) { }
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any ) {
-    this.dataSource = this.data;
-    
-  }
 
   ngOnInit(): void {
-    
+    this.bindTable();
+    this.defaultForm();
   }
 
+  defaultForm() {
+    this.bankForm = this.fb.group({
+      "createdBy": 0,
+      "modifiedBy": 0,
+      "createdDate": new Date(),
+      "modifiedDate": new Date(),
+      "isDeleted": false,
+      "id":this.editFlag ? this.editObj.id :   0,
+      "bankName": this.editFlag ? this.editObj.bankName : ""
+    })
+  }
+  bindTable() {
+    this.api.setHttp( 'get', 'api/BankRegistration/GetAll', false, false, false, 'baseURL');
+    this.api.getHttp().subscribe({
+      next: (res: any) => {
+        res.statusCode == 200 ? this.dataSource = res.responseData : this.dataSource = [];
+      }
+    })
+  }
+  onDelete(id:number){
+    this.api.setHttp('delete','api/BankRegistration?id='+id,false, false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : (res:any)=>{
+        this.mat.open(res.statusMessage,'ok');
+        this.bindTable();
+      }
+    })
+  }
 
+  onEdit(data:any){
+    this.editObj = data;
+    this.editFlag = true;
+    this.defaultForm();
+  }
+
+  onCancel(){
+    this.editFlag = false;
+  }
+
+  onSubmit() {
+    let obj = this.bankForm.value;
+    this.api.setHttp( this.editFlag ? 'put' : 'post', 'api/BankRegistration', false, obj, false, 'baseURL');
+    this.api.getHttp().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.mat.open(res.statusMessage, 'ok');
+        this.bindTable();
+        // this.bankForm.reset();
+        this.editFlag = false;
+      }
+    })
+    this.bankForm.reset();
+    this.defaultForm();
+  }
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {sr_no: 1, Bank_Name: '',action:''},
+  { sr_no: 1, Bank_Name: '', action: '' },
 ];
 export interface PeriodicElement {
   sr_no: number;
--- src/app/partial/master/bank-branch-registration/bank-branch-registration.component.ts
+++ src/app/partial/master/bank-branch-registration/bank-branch-registration.component.ts
@@ -12,45 +12,42 @@ import { AddBankBranchRegistrationComponent } from './add-bank-branch-registrati
 })
 export class BankBranchRegistrationComponent implements OnInit {
 
-  displayedColumns: string[] = ['sr_no', 'Bank_Name', 'Branch_Name','IFSC_Code','Action'];
+  displayedColumns: string[] = ['sr_no', 'Bank_Name', 'Branch_Name', 'IFSC_Code', 'Action'];
   dataSource = ELEMENT_DATA;
   bankNames = new Array();
-  regForm! : FormGroup;
-  editObj:any;
-  id="";
-  editFlag : boolean = false;
+  regForm!: FormGroup;
+  filterForm! : FormGroup
+  editObj: any;
+  id = "";
+  editFlag: boolean = false;
 
-  constructor(public dialog: MatDialog, private api : CallApiService, private fb : FormBuilder, private mat : MatSnackBar) {}
+  constructor(public dialog: MatDialog, private api: CallApiService, private fb: FormBuilder, private mat: MatSnackBar) { }
 
   ngOnInit(): void {
     this.bindTable();
     this.bankNameDropDown();
     this.defaultForm();
+    this.filterData();
   }
- 
+
   openDialog() {
-    const dialogRef = this.dialog.open(AddBankBranchRegistrationComponent,{
-      width:'50%',
-      data : this.dataSource
-      
-      
-      
+    const dialogRef = this.dialog.open(AddBankBranchRegistrationComponent, {
+      width: '50%',
+      data: this.dataSource
     });
-
-    // dialogRef.afterClosed().subscribe(result => {
-    //   console.log(`Dialog result: ${result}`);
-    // });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
-  bindTable(){
+  bindTable() {
     this.api.setHttp('get', 'api/BankBranchRegistration/GetAll', false, false, false, 'baseURL');
     this.api.getHttp().subscribe({
       next: (res: any) =>
         res.statusCode == 200 ? this.dataSource = res.responseData : this.dataSource = []
     })
-
   }
-  defaultForm(){
+  defaultForm() {
     this.regForm = this.fb.group({
       "createdBy": 0,
       "modifiedBy": 0,
@@ -58,62 +55,76 @@ export class BankBranchRegistrationComponent implements OnInit {
       "modifiedDate": new Date(),
       "isDeleted": false,
       "id": 0,
-      "bankId": this.editFlag ? this.editObj.bankId : "",
+      "bankId": this.editFlag ? this.editObj.id : 0,
       "branchName": this.editFlag ? this.editObj.branchName : "",
       "ifsC_Code": this.editFlag ? this.editObj.ifsC_Code : ""
     })
   }
-  //----------------------------- Dropdown Starts-------------------------------------//
-  bankNameDropDown(){
-    this.api.setHttp('get','api/CommonDropDown/GetBankRegistration',false,false,false,'baseURL');
-    this.api.getHttp().subscribe({
-      next : (res:any) =>
-      res.statusCode == 200 ? this.bankNames = res.responseData : this.bankNames = []
+  filterData(){
+    this.filterForm = this.fb.group({
+      "id" : 0,
+      "branch" : ""
     })
-    
+  }
+  onSearch(){
+    let text = this.filterForm.value.id;
+    this.api.setHttp('get','api/BankRegistration/GetAll?BankName='+text,false,false,false,'baseURL');
+    this.api.getHttp().subscribe({
+      next : (res:any)=>{
+        this.dataSource = res.responseData
+      }
+    })
+  }
+  //----------------------------- Dropdown Starts-------------------------------------//
+  bankNameDropDown() {
+    this.api.setHttp('get', 'api/CommonDropDown/GetBankRegistration', false, false, false, 'baseURL');
+    this.api.getHttp().subscribe({
+      next: (res: any) =>
+        res.statusCode == 200 ? this.bankNames = res.responseData : this.bankNames = []
+    })
+
   }
   //----------------------------- Dropdown Ends-------------------------------------//
-  onEdit(data:any){
-    this.bankNameDropDown();
+  onEdit(data: any) {
     this.editFlag = true;
+    this.bankNameDropDown();
     this.editObj = data;
     this.defaultForm();
     console.log(data);
-    
-
   }
-  onDelete(id:any){
+  onDelete(id: any) {
+    console.log(id);
     let obj = {
       "id": id,
       "modifiedBy": 0,
       "modifiedDate": new Date()
     }
-    this.api.setHttp('delete','api/BankBranchRegistration',false,obj,false,'baseURL');
+    this.api.setHttp('delete', 'api/BankBranchRegistration', false, obj, false, 'baseURL');
     this.api.getHttp().subscribe({
-      next : (res:any) =>{
-        this.mat.open(res.statusMessage,'ok');
+      next: (res: any) => {
+        this.mat.open(res.statusMessage, 'ok');
         this.bindTable();
       }
     })
-
   }
 
-  onSubmit(){
+  onSubmit() {
     let obj = this.regForm.value;
     console.log(obj);
-    this.api.setHttp('post','api/BankBranchRegistration',false,obj,false,'baseURL');
+    this.api.setHttp(this.editFlag ? 'put':'post', 'api/BankBranchRegistration', false, obj, false, 'baseURL');
     this.api.getHttp().subscribe({
-      next : (res:any)=>{
-        this.mat.open(res.statusMessage,'ok');
+      next: (res: any) => {
+        this.mat.open(res.statusMessage, 'ok');
         this.bindTable();
+       
         this.regForm.reset();
       }
     })
+    this.editFlag = false;
   }
-
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {sr_no: 1, Bank_Name: '',Branch_Name:'',IFSC_Code:''},
+  { sr_no: 1, Bank_Name: '', Branch_Name: '', IFSC_Code: '' },
 ];
 export interface PeriodicElement {
   sr_no: number;
