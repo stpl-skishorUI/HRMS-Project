c7e413fb1b9eade49f454aea7603dd3597d260a8
Sanket <panchmind1@gmail.com>
2022-10-03 17:21:18 +0530

    emp salary registration and add new sal page design done.

--- src/app/partial/register/employee-salary-details/employee-salary-details.component.ts
+++ src/app/partial/register/employee-salary-details/employee-salary-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { AddSalaryTypeComponent } from '../../master/salary-type-registration/add-salary-type/add-salary-type.component';
+import { AddNewSalaryComponent } from './add-new-salary/add-new-salary.component';
+
 
 @Component({
   selector: 'app-employee-salary-details',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeSalaryDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
 
+
+  addnewsalary() {
+    const dialogRef = this.dialog.open(AddNewSalaryComponent,{
+      width:'50%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  displayedColumns: string[] = ['srno', 'employee_name', 'gross_salary', 'effective_date','actions'];
+  dataSource = ELEMENT_DATA;
 }
+
+export interface PeriodicElement {
+  srno: number;
+  employee_name: string;
+  gross_salary: number;
+  effective_date: string;
+  actions: any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, employee_name: 'Ram Chavan', gross_salary: 500000, effective_date: '03-10-2022',actions:''}
+];
