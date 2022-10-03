f3fea86407597b79c6f54a0bcc60d4905cf61fd6
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-10-03 17:43:13 +0530

    redesign page

--- src/app/partial/register/assign-reporting-person/assign-reporting-person.component.ts
+++ src/app/partial/register/assign-reporting-person/assign-reporting-person.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign-reporting-person.component.scss']
 })
 export class AssignReportingPersonComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'Reporting_Person_Name', 'Actions','Approve_Right'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: '	Navin Boddhul', Reporting_Person_Name:'Medical', Actions: '',},
+];
+
+export interface PeriodicElement {
+  position: number;
+  name: string;
+  Reporting_Person_Name: any;
+  Actions: string;
+}
+
