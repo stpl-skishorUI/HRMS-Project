a4ac380704866657846484db789d93cc0cfcd78a
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2022-10-03 19:09:42 +0530

    commit

--- src/app/partial/master/company-registration/add-company/add-company.component.ts
+++ src/app/partial/master/company-registration/add-company/add-company.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CallApiService } from 'src/app/core/services/call-api.service';
+import { AddSalaryTypeComponent } from '../../salary-type-registration/add-salary-type/add-salary-type.component';
 
 @Component({
   selector: 'app-add-company',
@@ -11,13 +13,23 @@ export class AddCompanyComponent implements OnInit {
 
   companyRegistrationForm !: FormGroup;
   dataSource = new Array();
-  imgURL : string = "https://image.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg";
+  // imgURL : string = "https://image.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg";
+  organizationArr = new Array();
+  editFlag : boolean = false;
 
-  constructor(private fb: FormBuilder, private service: CallApiService) { }
+  @ViewChild('img') img !: string;
+
+  constructor(private fb: FormBuilder, private service: CallApiService, public dialogRef: MatDialogRef<AddSalaryTypeComponent>, @Inject(MAT_DIALOG_DATA) public data: any,) { }
 
   ngOnInit(): void {
     this.formField();
-    // this.getTableData();
+    this.getOrganizationData();
+    this.getTableData();
+  
+    if(this.editFlag){
+      this.onEdit(this.data);
+    }
+
   }
 
   formField() {
@@ -26,15 +38,15 @@ export class AddCompanyComponent implements OnInit {
       "modifiedBy": 0,
       "createdDate": new Date(),
       "modifiedDate": new Date(),
-      "isDeleted": false,
-      "id": 57,
-      "organizationId": 0,
+      "isDeleted": true,
+      "id": 0,
+      "organizationId": [''],
       "companyName": [''],
       "contactNo": [''],
       "address": [''],
       "website": [''],
       "emailId": [''],
-      "companyLogo": [this.imgURL],
+      "companyLogo": [''],
       "aboutUs": ['']
     })
   }
@@ -43,44 +55,94 @@ export class AddCompanyComponent implements OnInit {
     this.service.setHttp('get', 'api/CompanyRegistration/GetAllCompanies', false, false, false, "baseURL");
     this.service.getHttp().subscribe({
       next: (res: any) => {
+        // console.log(res);
         this.dataSource = res.responseData;
-        console.log(res);
+      
+      }
+    })
+  }
+
+  getOrganizationData(){
+    this.service.setHttp('get','api/CommonDropDown/GetOrganization', false, false, false, "baseURL");
+    this.service.getHttp().subscribe({
+      next:(res:any)=>{
+        this.organizationArr = res.responseData;
+        // console.log(res);
       }
     })
   }
 
   onSubmit() {
-    let formValue = this.companyRegistrationForm.value;
-    console.log(formValue);
-    this.service.setHttp('post', 'api/CompanyRegistration', false, formValue, false, "baseURL");
+    if(!this.editFlag){
+      let formValue = this.companyRegistrationForm.value;
+    // console.log(formValue);
+    this.service.setHttp('post','api/CompanyRegistration', false, formValue, false, 'baseURL');
     this.service.getHttp().subscribe({
       next: (res: any) => {
         this.getTableData();
-
       }
+    })
+    }
+    else{
+      let editValue = this.companyRegistrationForm.value;
+      this.service.setHttp('put','api/CompanyRegistration', false, editValue, false, 'baseURL');
+      this.service.getHttp().subscribe({
+        next: (res:any)=>{
+          this.getTableData();
+        }
+      })
+    }
+  }
+
+  onEdit(obj:any){
+    this.editFlag = true;
+    this.companyRegistrationForm.patchValue({
+      // "createdBy": 0,
+      // "modifiedBy": 0,
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      isDeleted: true,
+      id: obj.id,
+      organizationId: obj.organizationId,
+      companyName: obj.companyName,
+      contactNo: obj.contactNo,
+      address: obj.address,
+      website: obj.website,
+      emailId: obj.emailId,
+      companyLogo: obj.companyLogo,
+      aboutUs: obj.aboutUs
     })
   }
 
-  // onSubmit(){
-  //   let formData = new FormData();
-  //   formData.append('FolderName', 'example');
-  //   formData.append('DocumentType','jpg,png,jpeg');
-  //   formData.append('UploadDocPath',this.companyRegistrationForm.value.formField);
-
-  //   this.service.setHttp('post','api/CompanyRegistration',false, formData, false, "baseURL");
+  // uploadImg(event:any, flag: string){
+  //   // let name = event.target.value.split('.')[0];
+  //   console.log(event);
+  //   let formImg = new FormData();
+  //   this.service.setHttp('post','HRMS/DocumentMaster/UploadFile', false, formImg, false, 'baseURL');
   //   this.service.getHttp().subscribe({
-  //     next:(res:any)=>{
-  //       console.log(res);
-
-
-  //       this.postApi();
+  //     next: (res:any)=>{
+  //       // console.log(res);
+  //       if(res.statusCode == 200){
+  //         this.onSubmit();
+  //       }
   //     }
   //   })
   // }
 
+  uploadImg(){
+    let formData = new FormData();
+    formData.append('FolderName', 'example');
+    formData.append('DocumentType','jpg,png,jpeg');
+    formData.append('UploadDocPath',this.companyRegistrationForm.value.formField.companyLogo);
 
+    this.service.setHttp('post','HRMS/DocumentMaster/UploadFile',false, formData, false, "baseURL");
+    this.service.getHttp().subscribe({
+      next:(res:any)=>{
+        console.log(res);
+        this.onSubmit();
+      }
+    })
+  }
 
-
-
-
+  
 }
