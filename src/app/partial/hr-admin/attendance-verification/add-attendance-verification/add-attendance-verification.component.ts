ff5e0bb6d0653b1129ffc8ac7e2969039795725e
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-10-04 15:03:04 +0530

    new create attendance verification page design

---
+++ src/app/partial/hr-admin/attendance-verification/add-attendance-verification/add-attendance-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-attendance-verification',
+  templateUrl: './add-attendance-verification.component.html',
+  styleUrls: ['./add-attendance-verification.component.scss']
+})
+export class AddAttendanceVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
