9223807aef87ab52db010a35a6de59c3220a2d28
Sanket <panchmind1@gmail.com>
2022-10-04 10:05:34 +0530

    employee registration  page design.

---
+++ src/app/partial/register/employee-registration/add-employee-details/add-employee-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee-details',
+  templateUrl: './add-employee-details.component.html',
+  styleUrls: ['./add-employee-details.component.scss']
+})
+export class AddEmployeeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
