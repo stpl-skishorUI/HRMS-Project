7be68b8b86d1f9e2828345850871f70663933cc7
Sanket <panchmind1@gmail.com>
2022-10-04 14:09:27 +0530

    advance payment page design.

---
+++ src/app/partial/accountant/advance-payment/add-new-payment/add-new-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-payment',
+  templateUrl: './add-new-payment.component.html',
+  styleUrls: ['./add-new-payment.component.scss']
+})
+export class AddNewPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
