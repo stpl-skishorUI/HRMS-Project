59fa42d79732418a38f16de166b7a31fc121ee85
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-10-04 17:29:41 +0530

    new create offer letter page design

--- src/app/partial/hr-admin/offer-letter/offer-letter.component.ts
+++ src/app/partial/hr-admin/offer-letter/offer-letter.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-letter.component.scss']
 })
 export class OfferLetterComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'Contact_No', 'Company','Department_Name','Designation_Name','Registration',];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: '', Contact_No:'', Company: '',Department_Name:'',Designation_Name:'',Registration:'',},
+ 
+];
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  Contact_No: any;
+  Company: any;
+  Department_Name: any;
+  Designation_Name: any;
+  Registration: any;
 }
