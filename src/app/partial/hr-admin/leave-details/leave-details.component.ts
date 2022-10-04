3b2287b81673f97113b8c6b75835a87bf739e97a
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-10-04 12:45:39 +0530

    new design leave details  page

--- src/app/partial/hr-admin/leave-details/leave-details.component.ts
+++ src/app/partial/hr-admin/leave-details/leave-details.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-details.component.scss']
 })
 export class LeaveDetailsComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'Employee_Name', 'Total_Leaves', 'Taken_Leaves', 'Remaining_Leaves'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: '', Employee_Name:'' , Total_Leaves: 'H', Taken_Leaves:'',Remaining_Leaves:'',},
+  
+];
+
+export interface PeriodicElement {
+  position: number;
+  name: any;
+  Employee_Name: any;
+  Total_Leaves: any;
+  Taken_Leaves: any;
+  Remaining_Leaves: any;
 }
