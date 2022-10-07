ca8ddb1d72c80631b15b5d8edc0808ea71e8ad7a
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-10-07 11:06:30 +0530

    changes done in salary type

--- src/app/partial/master/salary-type-registration/add-salary-type/add-salary-type.component.ts
+++ src/app/partial/master/salary-type-registration/add-salary-type/add-salary-type.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CallApiService } from 'src/app/core/services/call-api.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -13,6 +13,7 @@ export class AddSalaryTypeComponent implements OnInit {
   isPercentageArray = [{ id: 0, name: 'Yes' }, { id: 1, name: 'No' }];
   salaryForm!: FormGroup;
   editFlag: boolean = false;
+
   constructor(private service: CallApiService, private fb: FormBuilder, public dialogRef: MatDialogRef<AddSalaryTypeComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private snack: MatSnackBar) { }
 
   ngOnInit(): void {
@@ -22,7 +23,7 @@ export class AddSalaryTypeComponent implements OnInit {
       this.OnEdit(this.data);
     }
   }
-  //--------------------------------------------------------------- dropDown Starts---------------------------------------------
+  //--------------------------------------------------------------- dropDown Starts---------------------------------------------------
   getCompanyNameDropdown() {
     this.service.setHttp('get', 'api/CommonDropDown/GetCompany', false, false, false, 'baseURL');
     this.service.getHttp().subscribe({
@@ -36,25 +37,38 @@ export class AddSalaryTypeComponent implements OnInit {
   }
   //--------------------------------------------------------------- dropDown Ends---------------------------------------------------
 
-  //--------------------------------------------------------------- Form Starts---------------------------------------------
+  //--------------------------------------------------------------- Form Starts-----------------------------------------------------
   getFormData() {
     this.salaryForm = this.fb.group({
       "createdBy": 0,
       "modifiedBy": 0,
-      createdDate: new Date(),
-      modifiedDate: new Date(),
+      "createdDate":new Date(),
+      "modifiedDate": new Date(),
       "isDeleted": true,
       "id": 0,
-      companyId: [''],
-      salary_Component: [''],
-      isPercentage: [],
-      value: []
+      companyId:0,
+      companyName:[''],
+      salary_Component:[''],
+      isPercentage: [''],
+      value:0
     })
+    // "createdBy": 0,
+// "modifiedBy": 0,
+// "createdDate": "2022-10-07T05:21:41.758Z",
+// "modifiedDate": "2022-10-07T05:21:41.758Z",
+// "isDeleted": true,
+// "id": 0,
+// "companyId": 0,
+// "companyName": "string",
+// "salary_Component": "string",
+// "isPercentage": true,
+// "value": 0
 
   }
+  //--------------------------------------------------------------- Form Submit Ends------------------------------------------------------
 
-  //--------------------------------------------------------------- Form Ends--------------------------------------------
 
+  //--------------------------------------------------------------- Form Submit starts------------------------------------------------------
   onSubmit() {
     let radioValues = this.salaryForm.value.isPercentage;
     if (radioValues == 0) {
@@ -64,51 +78,53 @@ export class AddSalaryTypeComponent implements OnInit {
       this.salaryForm.controls['isPercentage'].setValue(false);
     }
 
-
     let postObj = this.salaryForm.value;
     if (this.editFlag == false) {
       console.log("postObj", postObj);
-      this.service.setHttp('post', 'api/SalaryType', false, postObj, false, 'baseURL');
+      this.service.setHttp('post', 'HRMS/SalaryType', false, postObj, false, 'baseURL');
       this.service.getHttp().subscribe({
         next: (res: any) => {
-          this.snack.open(res.statusMessage, "Ok");
+          this.snack.open(res.statusMessage, "Ok", { duration: 3000 });
         }
       })
-      // } else {
-      //   this.editFlag = true;
-      //   this.service.setHttp('put', 'api/SalaryType', false, postObj, false, 'baseURL');
-      //   this.service.getHttp().subscribe({
-      //     next: (res: any) => {
-      //       this.snack.open(res.statusMessage,'Ok');
-      //     }
-      //   })
+    } else {
+      this.editFlag = true;
+      this.service.setHttp('put', 'HRMS/SalaryType', false, postObj, false, 'baseURL');
+      this.service.getHttp().subscribe({
+        next: (res: any) => {
+          this.snack.open(res.statusMessage, 'Ok', { duration: 3000 });
+        }
+      })
     }
-
   }
+  //--------------------------------------------------------------- Form Submit Ends------------------------------------------------------
+ 
+   //---------------------------------------------------------------Edit Form Starts-----------------------------------------
   OnEdit(obj?: any) {
-    // this.editFlag = true;
-    let editObj=obj;
+    this.editFlag = true;
+    let editObj = obj;
     this.data = editObj;
-    editObj.isPercentage == 0?editObj.isPercentage=true :false 
+    console.log(" this.data", this.data);
+    console.log("editObj",editObj);
+    editObj.isPercentage == 0 ? editObj.isPercentage = true : false
     this.salaryForm.patchValue({
-      createdBy: 0,
-      modifiedBy: 0,
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      isDeleted: true,
-      id: 0,
-      companyId: obj.companyId,
+      "createdBy": 0,
+      "modifiedBy": 0,
+      "createdDate": new Date(),
+      "modifiedDate": new Date(),
+      "isDeleted": true,
+      "id": 0,
+      companyId:obj.companyId,
+      companyName:obj.companyName,
       salary_Component: obj.salary_Component,
-      isPercentage: obj.isPercentage,
+      isPercentage:+obj.isPercentage,
       value: obj.value
     })
     this.getCompanyNameDropdown();
 
   }
-
-
+  //-------------------------------------------------Edit Forms End----------------------------------------------------------------
 }
-
 
 
 
