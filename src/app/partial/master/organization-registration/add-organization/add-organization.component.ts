aae251c79fc582386821658cb53ab8702d8d28f4
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-10-07 14:51:39 +0530

    organization registration files upl

--- src/app/partial/master/organization-registration/add-organization/add-organization.component.ts
+++ src/app/partial/master/organization-registration/add-organization/add-organization.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CallApiService } from 'src/app/core/services/call-api.service';
 
 @Component({
   selector: 'app-add-organization',
@@ -6,10 +10,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-organization.component.scss']
 })
 export class AddOrganizationComponent implements OnInit {
-
-  constructor() { }
+  OrganizationRegForm!: FormGroup;
+  editFlag: boolean = false;
+  data1: any;
+  fileURl: any;
+  // imageURL:string="../../../../../assets/images/user.jpg";
+  imageURL: any;
+  // @ViewChild('img') img!: ElementRef;
+  img: any;
+  constructor(private service: CallApiService, private fb: FormBuilder, private snackbar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any) { this.data1 = data }
 
   ngOnInit(): void {
+    // this.imageURL ? this.data?.orgLogo : "/assets/images/user.jpg";
+    console.log("Image Loaded", this.data);
+    this.formData();
   }
 
+  formData() {
+    console.log(" form valuee selectedd:",this.data1 );
+    this.data ? this.editFlag = true : ''
+    this.OrganizationRegForm = this.fb.group({
+      "createdBy": 0,
+      "modifiedBy": 0,
+      "createdDate": new Date(),
+      "modifiedDate": new Date(),
+      "isDeleted": false,
+      id: this.editFlag ? this.data1.id : 0,
+      orgName: this.editFlag ? this.data1.orgName : "",
+      contactNo: this.editFlag ? this.data1.contactNo : "",
+      address: this.editFlag ? this.data1.address : "",
+      website: this.editFlag ? this.data1.website : "",
+      emailId: this.editFlag ? this.data1.emailId : "",
+      orgLogo: this.editFlag ? this.data1.orgLogo : "",
+      aboutUs: this.editFlag ? this.data1.aboutUs : ""
+    });
+    this.imageURL = this.editFlag? this.data1.orgLogo : "/assets/images/user.jpg";
+  }
+  //************************Start Submit Logic Here**********************/
+  onSubmit() {
+    let data = this.OrganizationRegForm.value;
+    this.imageURL = data.orgLogo;
+    // data.orgLogo = this.imageURL;
+    console.log(data);
+    if (!this.editFlag) {
+      this.service.setHttp('post', 'HRMS/Orgnization/SaveOrg', false, data, false, 'baseURL');
+      this.service.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == '200') {
+            this.snackbar.open(res.statusMessage, 'ok');
+            // this.bindTable();
+          }
+        }
+      })
+    }
+    else {
+      this.editFlag = true;
+      this.service.setHttp('put', 'HRMS/Orgnization/UpdateOrg', false, data, false, 'baseURL');
+      this.service.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == '200') {
+            this.snackbar.open(res.statusMessage, 'ok');
+            // this.profileImg = res.orgLogo;
+          }
+        }
+      })
+    }
+  }
+  //************************End Submit Logic Here**********************/
+
+  //***************************Start Upload Img*******************************/
+  selectImg() {
+    this.img.nativeElement.click();
+  }
+
+  uploadImg(event: any) {
+    let finalValue = event.target.value;
+    // console.log(" file selected:", finalValue);
+    let extension = event.target.value.split('.')[1];
+    extension = extension.toLowerCase();
+    if (extension == 'jpg' || extension == 'png') {
+      const file = event.target.files[0];
+      // console.log(event);
+      if (file.size > 1000000) {
+        this.snackbar.open('Upload another Image', 'Ok');
+        this.img.nativeElement.value = '';
+        return
+      }
+      let readImg = new FileReader();
+      readImg.onload = (event: any) => {
+        this.fileURl = event.target.result;
+      }
+      readImg.readAsDataURL(file);
+      this.OrganizationRegForm.value.orgLogo = file;
+    }
+    let formData = new FormData();
+    formData.append('FolderName', 'D');
+    formData.append('DocumentType', 'png,jpg');//set default extension if you required
+    formData.append('UploadDocPath', this.OrganizationRegForm.value.orgLogo);
+    console.log(this.formData);
+    this.service.setHttp('post', 'HRMS/DocumentMaster/UploadFile', false, formData, false, "baseURL");
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        console.log("*********************", res);
+        this.imageURL = res.responseData;
+        if (res.statusCode == 200) {
+          // this.imageURL = res.responseData;
+          this.snackbar.open(res.statusMessage, 'Ok');
+        }
+      }
+    })
+  }
+  //*************************End Upload Img*************************************** */
 }
--- src/app/partial/master/organization-registration/organization-registration.component.ts
+++ src/app/partial/master/organization-registration/organization-registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddOrganizationComponent } from './add-organization/add-organization.component';
+import { CallApiService } from 'src/app/core/services/call-api.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-organization-registration',
@@ -8,24 +11,131 @@ import { AddOrganizationComponent } from './add-organization/add-organization.co
   styleUrls: ['./organization-registration.component.scss']
 })
 export class OrganizationRegistrationComponent implements OnInit {
-  displayedColumns: string[] = ['srno', 'organization_logo', 'organization_name', 'email','address','action'];
+  filterForm!: FormGroup;
+  displayedColumns: string[] = ['srno', 'organization_logo', 'organization_name', 'email', 'address', 'action'];
   dataSource = ELEMENT_DATA;
-  constructor(public dialog: MatDialog) { }
+  editFlag: boolean = false;
+  totalCount = 0;
+  pageSize = 10;
+  currentPage = 0;
+  //  dataSource:any;
+  constructor(public dialog: MatDialog, private service: CallApiService, public fb: FormBuilder,private snackbar:MatSnackBar) { }
 
   ngOnInit(): void {
+    this.bindTable();
+    this.filterMethod();
   }
 
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(AddOrganizationComponent, {
-      width: '50%',
-      enterAnimationDuration,
-      exitAnimationDuration,
+  filterMethod() {
+    this.filterForm = this.fb.group({
+      orgName: [''],
+    })
+  }
+  //***********Start Dialog Box*******************/
+  addOrganization(obj?: any) {
+    const dialogRef = this.dialog.open(AddOrganizationComponent, {
+      width: '40%',
+      height: '80%',
+      data: obj,
+      disableClose: true
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      this.bindTable();
     });
   }
+  // openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+  //     this.dialog.open(AddOrganizationComponent, {
+  //       width: '50%',
+  //       enterAnimationDuration,
+  //       exitAnimationDuration,
+  //     });
 
+  //   }
+  //***********End Dialog Box*******************/
+  //***************Start Bind Table Here*******************/
+  bindTable() {
+   
+    this.service.setHttp('get', 'HRMS/Orgnization/GetAllOrgByPagination?pageno=' + (this.currentPage + 1) + '&pagesize=10', false, false, false, 'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+       this.dataSource = res.responseData;
+       this.dataSource.map((cr: any)=>{
+        cr.orgLogo = "http://hrmssvr.erpguru.in/Uploads" + cr.orgLogo.split('Uploads')[1];
+       })
+        this.totalCount = res.responseData1.pageCount;
+       
+        console.log(res);
+      }
+    })
+  }
+
+
+  // getTableData() {
+  //   this.service.setHttp('get', 'api/CompanyRegistration/GetAllCompanies?pageno='+(this.currentPage + 1)+'&pagesize=10', false, false, false, "baseURL");
+  //   this.service.getHttp().subscribe({
+  //     next: (res: any) => {
+  //       this.dataSource = res.responseData;
+  //       this.dataSource.map((cr: any)=>{
+  //         cr.companyLogo = "http://hrmssvr.erpguru.in/Uploads" + cr.companyLogo.split('Uploads')[1];
+  //        })
+  //       this.totalCount = res.responseData1.pageCount;
+  //       this.snackbar.open(res.statusMessage, 'Ok');
+  //       console.log(res);
+  //     }
+  //   })
+  // }
+
+
+
+
+  //***************End Bind Table Here*******************/
+  //******************Filter Data Submit Logic Start************************/
+  filterData() {
+    let orgType = this.filterForm.value.orgName;
+    console.log(orgType);
+    this.service.setHttp('get','HRMS/Orgnization/GetAllOrgByPagination?name=' + orgType, false, false, false,
+      'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == '200') {
+          this.snackbar.open(res.statusMessage,'ok');
+          // console.log('aaa', res);
+          // let filterArray: any[] = [res.responseData];
+          this.dataSource = res.responseData;
+          this.filterForm.reset();
+        }
+      }
+    })
+  }
+  //**********************Filter Data Submit Logic End**********************
+  //***************Start Delete Logic********************/
+  onDelete(id: number) {
+    // let obj ={
+    //   "id": id,
+    //   "modifiedBy": 0,
+    //   "modifiedDate": "2022-10-03T13:25:44.285Z"
+    // }
+    // this.service.setHttp('delete','api/Orgnization/id',false,obj,false,'baseURL');
+    this.service.setHttp('delete', 'HRMS/Orgnization/DeleteOrg?id=' + id, false, false, false, 'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        this.snackbar.open(res.statusMessage,'ok');
+        // this.mat.open(res.statusMessage,'ok');
+        this.bindTable();
+      }
+    })
+  }
+  //****************End Delete Logic**********************/
+  //************Start Handle page for Pagination***************/
+  handlePageEvent(event: any) {
+    this.currentPage = event.pageIndex;
+    this.bindTable();
+  }
+  //************End Handle page for Pagination***************/
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, organization_logo: '', organization_name: 'shaurya', email: 'H',address:'Pune-11',action:''},
+  { srno: 1, organization_logo: '', organization_name: 'shaurya', email: 'H', address: 'Pune-11', action: '' },
 ];
 export interface PeriodicElement {
   srno: number;
@@ -35,3 +145,42 @@ export interface PeriodicElement {
   address: string;
   action: any;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//   displayedColumns: string[] = ['srno', 'organization_logo', 'organization_name', 'email','address','action'];
+//   dataSource = ELEMENT_DATA;
+//   constructor(public dialog: MatDialog) { }
+
+//   ngOnInit(): void {
+//   }
+
+//   openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+//     this.dialog.open(AddOrganizationComponent, {
+//       width: '50%',
+//       enterAnimationDuration,
+//       exitAnimationDuration,
+//     });
+//   }
+
+// }
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {srno: 1, organization_logo: '', organization_name: 'shaurya', email: 'H',address:'Pune-11',action:''},
+// ];
+// export interface PeriodicElement {
+//   srno: number;
+//   organization_logo: string;
+//   organization_name: string;
+//   email: string;
+//   address: string;
+//   action: any;
+// }
