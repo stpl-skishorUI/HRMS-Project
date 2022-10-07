4049afeead4b7ae67b07638b69efdd7be5abd70d
Pradip-ShauryaTechnosoft <pradip.ganjale@shauryatechnosoft.com>
2022-10-07 11:28:01 +0530

    change on designation

--- src/app/partial/master/designation-registration/designation-registration.component.ts
+++ src/app/partial/master/designation-registration/designation-registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { AddDesignationComponent } from './add-designation/add-designation.component';
+import { CallApiService } from 'src/app/core/services/call-api.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-designation-registration',
@@ -9,24 +12,130 @@ import { AddDesignationComponent } from './add-designation/add-designation.compo
 })
 export class DesignationRegistrationComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) {}
+  filtterForm!: FormGroup;
+  companyDropdown: any[] = [];
+  departmentDropdown: any[] = [];
+  designationDropdown: any[] = [];
+  filtterCompanyId!:number;
+  filtterDepartmentId!:number;
+  filtterDesignationText: string='';
+
+  totalCount: any;
+  currentPage: number = 1;
+
+  constructor(public dialog: MatDialog, private service: CallApiService, private formBuilder: FormBuilder,private snack:MatSnackBar) { }
 
   ngOnInit(): void {
+    this.getTableData();
+    this.getCompanyDropdown();
+    this.fillterFormData();
+
   }
 
-  addDesignation() {
-    const dialogRef = this.dialog.open(AddDesignationComponent,{
-      width: '30%'
-    });
+ //-----------------------------------Filter Data------------------------------------------//
+  fillterFormData() {
+    this.filtterForm = this.formBuilder.group({
+      filtterCompany: [''],
+      filtterDepartment: [''],
+      filtterDesignation: ['']
+    })
+  }
+  fillterOnSubmit() {
+    let obj = this.filtterForm.value
+    this.filtterCompanyId = obj.filtterCompany;
+    this.filtterDepartmentId = obj.filtterDepartment;
+    this.filtterDesignationText = obj.filtterDesignation;
 
+    this.service.setHttp('get', 'HRMS/Designation/GetAllDesignationByPagination?pageno='+(this.currentPage)+'&pagesize=10&cId='+this.filtterCompanyId+'&dId='+this.filtterDepartmentId+'&searchText='+this.filtterDesignationText, false, false, false,
+    'baseURL');
+  this.service.getHttp().subscribe({
+    next: (res: any) => {         
+      this.dataSource = res.responseData;
+      console.log(this.dataSource);
+      // this.snack.open(res.statusMessage,"ok");
+      this.totalCount = res.responseData1.pageCount;
+    }
+  })
+  
+  
+  }
+  pageChanged(event: any) {
+    this.currentPage = event.pageIndex;
+    this.getTableData();
+  }
+ //-----------------------------------Filter Data------------------------------------------//
+  addDesignation(obj?: any) {
+    const dialogRef = this.dialog.open(AddDesignationComponent, {
+      width: '30%',
+      data: obj
+    });
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      this.getTableData(),
+      console.log(`Dialog result: ${result}`)
     });
   }
 
 
-  displayedColumns: string[] = ['srno', 'designation_name', 'department_name', 'company','action'];
+  displayedColumns: string[] = ['srno', 'designation_name', 'department_name', 'company', 'action'];
   dataSource = ELEMENT_DATA;
+
+  //-----------------------------------Table Binding------------------------------------------//
+  getTableData() {
+    this.service.setHttp('get', 'HRMS/Designation/GetAllDesignationByPagination', false, false, false,
+      'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {   
+        console.log("res",res);
+              
+        this.dataSource = res.responseData;
+        console.log(this.dataSource);
+        // this.snack.open(res.statusMessage,"ok");
+        this.totalCount = res.responseData.responseData2.totalCount;
+      }
+    })
+  }
+  //-----------------------------------Table Binding------------------------------------------//
+
+  //-----------------------------------Drop-Down------------------------------------------//
+  getCompanyDropdown() {
+    this.service.setHttp('get', 'api/CommonDropDown/GetCompany', false, false, false,
+      'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200 && res.responseData.length) {
+          this.companyDropdown = res.responseData;
+        }
+        this.getDepartmentDropdown();
+      }
+    })
+  }
+  getDepartmentDropdown() {
+    this.service.setHttp('get', 'api/CommonDropDown/GetDepartment', false, false, false,
+      'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        
+        if (res.statusCode == 200 && res.responseData.length) {
+          this.departmentDropdown = res.responseData;
+        }
+        this.getDesignationDropdown();
+      }
+    })
+  }
+  getDesignationDropdown() {
+    this.service.setHttp('get', 'api/CommonDropDown/GetDesignation', false, false, false,
+      'baseURL');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {        
+        if (res.statusCode == 200 && res.responseData.length) {
+          this.designationDropdown = res.responseData;
+        }
+      }
+    })
+  }
+  //-----------------------------------Drop-Down------------------------------------------//
+
 }
 export interface PeriodicElement {
   srno: number;
@@ -34,9 +143,10 @@ export interface PeriodicElement {
   department_name: string;
   company: string;
   action: any;
-
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, designation_name: 'Html Designer', department_name: 'Software Development', company: 'shaurya',action:''}
+  { srno: 1, designation_name: 'Html Designer', department_name: 'Software Development', company: 'shaurya', action: '' }
 ];
+
+
