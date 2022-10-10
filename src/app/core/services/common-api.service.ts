476c7984fd53ee5d57b161b07f5ae72ce096181d
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2022-10-10 19:01:13 +0530

    Company Reg - updated

--- src/app/core/services/common-api.service.ts
+++ src/app/core/services/common-api.service.ts
@@ -6,26 +6,30 @@ import { CallApiService } from './call-api.service';
   providedIn: 'root'
 })
 export class CommonApiService {
+  resCompanies = new Array();
+  resOrganization = new Array();
 
   constructor(private apiService: CallApiService) { }
 
-  getCompanies(){
-    return new Observable(obj => {
-      this.apiService.setHttp('get', 'api/CommonDropDown/GetCompany?OrgId=1', true, false, false, 'baseURL');
+
+  getCompanies() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetCompany?OrgId=1', false, false, false, 'baseURL');
       this.apiService.getHttp().subscribe({
-        next: (res: any) => {
-          if (res.statusCode === "200") {
-            obj.next(res);
-          }
-          else{
-            obj.error(res);
-          }
-        },
-        error: (error: any) => {
-          console.log("Error is :",error );
-        }
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
       });
     });
   }
 
+  getOrganization() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('Post', 'api/CommonDropDown/GetOrganization', false, false, false, 'baseURL');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
+  }
+  
 }
