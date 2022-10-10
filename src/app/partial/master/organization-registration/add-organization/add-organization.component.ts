a8a9cee61b97f460370a04b5a895a442f266c044
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-10-10 17:56:13 +0530

    org reg upl

--- src/app/partial/master/organization-registration/add-organization/add-organization.component.ts
+++ src/app/partial/master/organization-registration/add-organization/add-organization.component.ts
@@ -37,20 +37,20 @@ export class AddOrganizationComponent implements OnInit {
       "modifiedDate": new Date(),
       "isDeleted": false,
       id: this.editFlag ? this.data1.id : 0,
-      orgName: this.editFlag ? this.data1.orgName : ["",Validators.required],
-      contactNo: this.editFlag ? this.data1.contactNo : ["",[Validators.required,Validators.pattern("^[6-9]{1}[0-9]{9}$")]],
-      address: this.editFlag ? this.data1.address : ["",Validators.required],
-      website: this.editFlag ? this.data1.website : ["",Validators.required],
-      emailId: this.editFlag ? this.data1.emailId : ["",[Validators.required,Validators.pattern("[a-z0-9]+@[a-z]+\.[a-z]{2,3}")]],
-      orgLogo: this.editFlag ? this.data1.orgLogo :  ["",Validators.required],
-      aboutUs: this.editFlag ? this.data1.aboutUs : ["",Validators.required],
+      orgName: this.editFlag ? this.data1.orgName : ['',Validators.required],
+      contactNo: this.editFlag ? this.data1.contactNo : ['',[Validators.required,Validators.pattern("^[6-9]{1}[0-9]{9}$")]],
+      address: this.editFlag ? this.data1.address : ['',Validators.required],
+      website: this.editFlag ? this.data1.website : ['',Validators.required],
+      emailId: this.editFlag ? this.data1.emailId : ['',[Validators.required,Validators.pattern("[a-z0-9]+@[a-z]+\.[a-z]{2,3}")]],
+      orgLogo: this.editFlag ? this.data1.orgLogo : [''],
+      aboutUs: this.editFlag ? this.data1.aboutUs : ['',Validators.required],
     });
     this.imageURL = this.editFlag? this.data1.orgLogo : "/assets/images/user.jpg";
   }
 //********************for Validation Handle*************************/
-  public hasError = (controlName: string, errorName: string) => {
-    return this.OrganizationRegForm.controls[controlName].hasError(errorName);
-  }
+ get f(){
+  return this.OrganizationRegForm.controls;
+ }
   //************************Start Submit Logic Here**********************/
   onSubmit() {
     let data = this.OrganizationRegForm.value;
