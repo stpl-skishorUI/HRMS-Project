8c5a43856c06925c9fb42f17a644d2e547d6655f
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2022-10-11 18:08:26 +0530

    bank-branch-reg updated

--- src/app/partial/master/bank-branch-registration/bank-branch-registration.component.ts
+++ src/app/partial/master/bank-branch-registration/bank-branch-registration.component.ts
@@ -5,7 +5,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { CallApiService } from 'src/app/core/services/call-api.service';
 import { CommonApiService } from 'src/app/core/services/common-api.service';
 import { AddBankBranchRegistrationComponent } from './add-bank-branch-registration/add-bank-branch-registration.component';
-
 @Component({
   selector: 'app-bank-branch-registration',
   templateUrl: './bank-branch-registration.component.html',
@@ -22,14 +21,12 @@ export class BankBranchRegistrationComponent implements OnInit {
   totalCount = 0;
   pageSize = 10;
   currentPage = 0;
- 
-  @ViewChild(FormGroupDirective) formGroupDirective!: FormGroupDirective;
 
-  constructor(private commonApi : CommonApiService,public dialog: MatDialog, private api: CallApiService, private fb: FormBuilder, private mat: MatSnackBar) { }
+  constructor(private commonApi: CommonApiService, public dialog: MatDialog, private api: CallApiService, private fb: FormBuilder, private mat: MatSnackBar) { }
   ngOnInit(): void {
+    this.defaultForm();
     this.bindTable();
     this.bankNameDropDown();
-    this.defaultForm();
     this.filterData();
   }
 
@@ -42,16 +39,15 @@ export class BankBranchRegistrationComponent implements OnInit {
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
-    this.bankNameDropDown();
-      // this.bindTable();
+      this.bankNameDropDown();
     });
   }
 
   bindTable() {
-    this.api.setHttp('get', 'HRMS/BankBranchRegistration/GetAllBankBranchByPagination?pageno=' + (this.currentPage + 1) + '&pagesize=10', false, false, false, 'baseURL');
+    this.api.setHttp('get', 'HRMS/BankBranchRegistration/GetAllBankBranchByPagination?pageno=' + (this.currentPage + 1) + '&pagesize=10&BankId=0', false, false, false, 'baseURL');
     this.api.getHttp().subscribe({
       next: (res: any) => {
-        res.statusCode == 200 && res.responseData.length ? (this.dataSource = res.responseData, this.totalCount = res.responseData1.pageCount) : this.dataSource = []
+        res.statusCode == 200 && res.responseData.length ? (this.dataSource = res.responseData, this.totalCount = res.responseData1.pageCount) : this.dataSource = [];
       }, error: (error: any) => {
         console.log("Error is : ", error);
       }
@@ -71,13 +67,13 @@ export class BankBranchRegistrationComponent implements OnInit {
       "modifiedDate": new Date(),
       "isDeleted": false,
       "id": this.editFlag ? this.editObj.id : 0,
-      "bankId": this.editFlag ? this.editObj.bankId : [0,Validators.required],
-      "branchName": this.editFlag ? this.editObj.branchName : ["",Validators.required],
-      "ifsC_Code": this.editFlag ? this.editObj.ifsC_Code : ["",[Validators.required,Validators.pattern('^[A-Z]{4}0[A-Z0-9]{6}$')]]
+      "bankId": this.editFlag ? this.editObj.bankId : [0, Validators.required],
+      "branchName": this.editFlag ? this.editObj.branchName : ["", Validators.required],
+      "ifsC_Code": this.editFlag ? this.editObj.ifsC_Code : ["", [Validators.required, Validators.pattern('^[A-Z]{4}0[A-Z0-9]{6}$')]]
     })
   }
 
-  get fc(){return this.regForm.controls};
+  get fc() { return this.regForm.controls };
 
   filterData() {
     this.filterForm = this.fb.group({
@@ -85,22 +81,20 @@ export class BankBranchRegistrationComponent implements OnInit {
       "branch": [""]
     })
   }
-//------For clear the fields when we change the Bank Name-----//
+  //------For clear the fields when we change the Bank Name-----//
   onChanges(event: any) {
     event.value ? (this.regForm.controls['branchName'].setValue(''), this.regForm.controls['ifsC_Code'].setValue('')) : '';
   }
-  
+
 
   onSearch() {
     let id = this.filterForm.value.id;
     let text = this.filterForm.value.branch;
-    // HRMS/BankBranchRegistration/GetAll?searchtxt=&BankId=1
-    // HRMS/BankBranchRegistration/GetAllBankBranchByPagination?searchtxt=Ravet&BankId=2
     this.api.setHttp('get', 'HRMS/BankBranchRegistration/GetAllBankBranchByPagination?searchtxt=' + text + '&BankId=' + id, false, false, false, 'baseURL');
     this.api.getHttp().subscribe({
       next: (res: any) => {
         console.log(res);
-        res.statusCode ==200 && res.responseData.length ? (this.dataSource = res.responseData,  this.totalCount = res.responseData1.pageCount) :this.dataSource =[] ;
+        res.statusCode == 200 && res.responseData.length ? (this.dataSource = res.responseData, this.totalCount = res.responseData1.pageCount) : this.dataSource = [];
       }, error: (error: any) => {
         console.log("Error is : ", error);
       }
@@ -114,7 +108,7 @@ export class BankBranchRegistrationComponent implements OnInit {
   }
 
   //----------------------------- Dropdown Starts-------------------------------------//
-  bankNameDropDown(){
+  bankNameDropDown() {
     this.commonApi.getBanks().subscribe({
       next: (res: any) => {
         res.statusCode == 200 && res.responseData.length ? this.bankNames = res.responseData : this.bankNames = [];
@@ -130,20 +124,20 @@ export class BankBranchRegistrationComponent implements OnInit {
     this.bankNameDropDown();
     this.defaultForm();
     this.fc['branchName'].setValidators([Validators.required]);
-    this.fc['ifsC_Code'].setValidators([Validators.required,Validators.pattern('^[A-Z]{4}0[A-Z0-9]{6}$')]);
+    this.fc['ifsC_Code'].setValidators([Validators.required, Validators.pattern('^[A-Z]{4}0[A-Z0-9]{6}$')]);
   }
 
-  onSubmit(clear:any) { 
-  let branchName = this.fc['branchName'].value;
+  onSubmit(clear: any) {
+    let branchName = this.fc['branchName'].value;
     if (!branchName.replace(/\s/g, '').length) { //string length is 0
       return;
     }
-    else{
+    else {
       let obj = this.regForm.value;
       this.api.setHttp(this.editFlag ? 'put' : 'post', 'HRMS/BankBranchRegistration', false, obj, false, 'baseURL');
       this.api.getHttp().subscribe({
         next: (res: any) => {
-          res.statusCode == 200 ? ( this.mat.open(res.statusMessage, 'ok') , this.bindTable(),  this.editFlag = false,clear.resetForm(), this.defaultForm()) :'';
+          res.statusCode == 200 ? (this.mat.open(res.statusMessage, 'ok'), this.bindTable(), this.editFlag = false, clear.resetForm(), this.defaultForm()) : '';
         }, error: (error: any) => {
           console.log("Error is : ", error);
         }
