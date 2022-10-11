3f287939bd49a82eadebf5d460d3c4487cbf6594
dhaneshwareegughane <gughanedhaneshwaree@gmail.com>
2022-10-11 12:32:51 +0530

    changes in leave type registration

--- src/app/partial/master/leave-type-registration/leave-type-registration.component.ts
+++ src/app/partial/master/leave-type-registration/leave-type-registration.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddLeaveTypeComponent } from './add-leave-type/add-leave-type.component';
 import { CallApiService } from 'src/app/core/services/call-api.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-leave-type-registration',
   templateUrl: './leave-type-registration.component.html',
@@ -13,37 +14,39 @@ export class LeaveTypeRegistrationComponent implements OnInit {
   displayedColumns: string[] = ['sr_no', 'LeaveType_Name', 'Half_Day', 'action'];
   // dataSource = ELEMENT_DATA;
   tableRespnse: any;
-  companyTypeResp:any;
-  filterleaveTypeForm!:FormGroup;
+  submitted = false;
+  companyTypeResp: any;
+  filterleaveTypeForm!: FormGroup;
   pageNo: number = 1;
   pageSize = 10;
   length: any;
 
   constructor(public dialog: MatDialog,
     private callAPIService: CallApiService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.filterleaveTypeForm=this.fb.group({
-      companytype:[''],
-      leaveType:['']
+    this.filterleaveTypeForm = this.fb.group({
+      companytype: [''],
+      leaveType: ['']
     })
     this.leaveRegistrationData();
     this.bindCompanytype();
   }
   openDialog(status: any, data?: any) {
-    console.log(data);
-    let leaveData={
-      data:data,
-      status:status
+    let leaveData = {
+      data: data,
+      status: status
     }
     const dialogRef = this.dialog.open(AddLeaveTypeComponent, {
       width: '30%',
-      data:leaveData
+      data: leaveData,
+      disableClose: true,
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      // console.log(`Dialog result: ${result}`);
       this.leaveRegistrationData();
       // this.dialogRef.close();
     });
@@ -53,10 +56,8 @@ export class LeaveTypeRegistrationComponent implements OnInit {
     this.callAPIService.setHttp('GET', 'api/CommonDropDown/GetCompany', false, false, false, 'baseURL');
     this.callAPIService.getHttp().subscribe({
       next: (resp: any) => {
-        console.log(resp);
         if (resp.statusCode == 200) {
           this.companyTypeResp = resp.responseData;
-          console.log(this.companyTypeResp);
         } else {
           // this.toastr.error(resp.statusMessage);
         }
@@ -66,42 +67,37 @@ export class LeaveTypeRegistrationComponent implements OnInit {
   }
 
   leaveRegistrationData() {
-    let formData=this.filterleaveTypeForm.value;
-    const obj = {
-      "pageno": this.pageNo,
-      "CompanyId":formData.companytype,
-      "searchText":formData.leaveType,
+    this. submitted = true;
+    let formData = this.filterleaveTypeForm.value;
+      const obj = {
+        "pageno": this.pageNo,
+        "CompanyId": formData.companytype,
+        "searchText": formData.leaveType,
+      }
+      this.callAPIService.setHttp('get', 'api/LeaveType/GetAllLeaveByPagination?pageno=' + obj.pageno + '&pagesize=10&CompanyId=' + obj.CompanyId + '&searchText=' + obj.searchText, false, false, false, 'baseURL');
+      this.callAPIService.getHttp().subscribe({
+        next: (resp: any) => {
+          // this.spinner.hide();
+          if (resp.statusCode == 200) {
+            this.tableRespnse = resp.responseData;
+            this.length = resp.responseData1.pageCount;
+            // this.toastr.success(resp.statusMessage);
+            // this.formGroupDirective.resetForm();
+          } else {
+            this.tableRespnse = [];
+            // this.toastr.error(resp.statusMessage);
+          }
+        },
+        // error: ((error: any) => { this.error.handelError(error.statusCode) })
+      })
     }
-    this.callAPIService.setHttp('get', 'api/LeaveType/GetAllLeaveByPagination?pageno=' + obj.pageno + '&pagesize=10&CompanyId='+obj.CompanyId+'&searchText='+obj.searchText, false, false, false, 'baseURL');
-    this.callAPIService.getHttp().subscribe({
-      next: (resp: any) => {
-        // this.spinner.hide();
-        console.log(resp);
-        if (resp.statusCode == 200) {
-          this.tableRespnse = resp.responseData;
-          this.length = resp.responseData1.pageCount;
-          // this.toastr.success(resp.statusMessage);
-          // this.formGroupDirective.resetForm();
-          // this.submitted = false;
-          // this.iseditbtn = false;
-          // this.bindTableData();
-
-        } else {
-          this.tableRespnse = [];
-          // this.toastr.error(resp.statusMessage);
-        }
-      },
-      // error: ((error: any) => { this.error.handelError(error.statusCode) })
-    })
-  }
-
+  
   paginationEvent(event: any) {
-    console.log(event);
     this.pageNo = event.pageIndex + 1;
     this.leaveRegistrationData()
   }
 
-  
+
 
 }
 
