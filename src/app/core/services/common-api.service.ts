007bb4cb37a4137612dbfc17811fee932be8de9c
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-10-11 11:45:27 +0530

    added common api

--- src/app/core/services/common-api.service.ts
+++ src/app/core/services/common-api.service.ts
@@ -11,10 +11,19 @@ export class CommonApiService {
 
   constructor(private apiService: CallApiService) { }
 
-
-  getCompanies() {
+  getOrganization() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', 'api/CommonDropDown/GetCompany?OrgId=1', false, false, false, 'baseURL');
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetOrganization', false, false, false, 'baseURL');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
+  }
+
+  getCompanies(ordId:number) {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetCompany?OrgId='+ordId, false, false, false, 'baseURL');
       this.apiService.getHttp().subscribe({
         next: (res: any) => {
           if (res.statusCode == "200") {
@@ -31,11 +40,41 @@ export class CommonApiService {
         // error: (e: any) => { obj.error(e) }
       });
     });
+  } 
+
+  getDeptByCompanyId(companyId:number) {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetDepartment?CompanyId='+companyId, false, false, false, 'baseURL');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
   }
 
-  getOrganization() {
+  getDesigByDeptId(companyId:number,deptId:number) {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', 'api/CommonDropDown/GetOrganization', false, false, false, 'baseURL');
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetDesignation?CompanyId='+companyId+'&DepartmentId='+deptId, false, false, false, 'baseURL');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
+  }
+
+  getBanks() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetBankRegistration', false, false, false, 'baseURL');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
+  }
+
+  getBranchesByBankId(bankId:number) {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', 'api/CommonDropDown/GetBankBranchRegistration?BankId=' + bankId, false, false, false, 'baseURL');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == "200") { obj.next(res); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
