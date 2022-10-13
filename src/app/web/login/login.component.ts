21a0da9437a876ce3b7296f3108f5adebb193134
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2022-10-13 12:52:47 +0530

    update - company registration page

--- src/app/web/login/login.component.ts
+++ src/app/web/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CallApiService } from 'src/app/core/services/call-api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  constructor() { }
+  loginForm !: FormGroup;
+  userName : string='';
+  password : any;
+  constructor(private fb : FormBuilder, private apiService : CallApiService) { }
 
   ngOnInit(): void {
+    this.formFeild();
+  }
+
+  formFeild(){
+    this.loginForm = this.fb.group({
+      userName : [''],
+      password : ['']
+    })
+
+  }
+
+  callApi(){
+    this.apiService.setHttp('get','HRMS/UserLogin/GetList?UserName='+this.userName+'&Password='+this.password, false, false, false, "baseURL");
+    
+  }
+
+  login(){
+    let formValue = this.loginForm.value;
+    console.log(formValue);
   }
 
 }
